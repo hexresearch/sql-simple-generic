8df93571fd2601540c7cab7fbfdbaa7f618839b8
Dmitry Zuikov <dzuikov@gmail.com>
2021-01-05 12:33:23 +0300

    almost there

--- DB/PG/Instances.hs
+++ DB/PG/Instances.hs
@@ -7,10 +7,14 @@ import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.FromField
 import Database.PostgreSQL.Simple.FromRow
 import Database.PostgreSQL.Simple.ToField
+import Data.Data
 import Data.Proxy
 import Data.Scientific
 import Data.String
+import Data.Text.Conversions
 import Data.Text (Text)
+import Data.Time.Calendar
+import GHC.Generics
 import GHC.TypeLits (KnownSymbol(..), symbolVal)
 import qualified Data.Text as Text
 import qualified GHC.TypeLits as T
@@ -28,11 +32,20 @@ instance ToField Symbol where
 instance FromField Symbol where
   fromField f x = Symbol <$> fromField f x
 
+instance FromField Volume where
+  fromField f x = (Volume <$> realToFrac) <$> fromField f x
+
 instance FromField BondShortName where
   fromField f x = BondShortName <$> fromField f x
 
 instance FromField BondFullName where
   fromField f x = BondFullName <$> fromField f x
+
+instance FromField TradeAggDate where
+  fromField f x = TradeAggDate <$> fromField f x
+
+instance FromField TradeAggVol where
+  fromField f x = TradeAggVol <$> fromField f x
 
 instance ToField Market where
   toField (Market x) = toField x
@@ -47,13 +60,14 @@ data From (table :: T.Symbol) cols = All
 data Rows cols
 
 type family SelectRowType a :: *
-
-type instance SelectRowType (From t (Rows [row])) = row
+type instance SelectRowType (From t (Rows [(a1,a2)])) = (a1,a2)
+type instance SelectRowType (From t (Rows [(a1,a2,a3)])) = (a1,a2,a3)
+type instance SelectRowType (From t (Rows [(a1,a2,a3,a4)])) = (a1,a2,a3,a4)
 
 instance ( HasTable q PostgreSQLEngine
          , HasColumns q
          , FromRow a
-         , SelectRowType q ~ a
+         , a ~ SelectRowType q
          ) => SelectStatement [a] q IO PostgreSQLEngine
       where
         select eng q = do
@@ -67,6 +81,32 @@ instance ( HasColumn t (Proxy a)
          , KnownSymbol t
          ) => HasColumns (From t (Rows [(a, b)])) where
   columns = const [ column (Proxy @t) (Proxy @a), column (Proxy @t) (Proxy @b) ]
+
+
+instance ( HasColumn t (Proxy a1)
+         , HasColumn t (Proxy a2)
+         , HasColumn t (Proxy a3)
+         , KnownSymbol t
+         ) => HasColumns (From t (Rows [(a1, a2, a3)])) where
+
+  columns = const [ column (Proxy @t) (Proxy @a1)
+                  , column (Proxy @t) (Proxy @a2)
+                  , column (Proxy @t) (Proxy @a3)
+                  ]
+
+
+instance ( HasColumn t (Proxy a1)
+         , HasColumn t (Proxy a2)
+         , HasColumn t (Proxy a3)
+         , HasColumn t (Proxy a4)
+         , KnownSymbol t
+         ) => HasColumns (From t (Rows [(a1, a2, a3, a4)])) where
+
+  columns = const [ column (Proxy @t) (Proxy @a1)
+                  , column (Proxy @t) (Proxy @a2)
+                  , column (Proxy @t) (Proxy @a3)
+                  , column (Proxy @t) (Proxy @a4)
+                  ]
 
 
 instance KnownSymbol t => HasTable (From t cols) e where
@@ -83,5 +123,32 @@ instance HasColumn "bondfullname" (Proxy Symbol) where
 
 instance HasColumn "bondfullname" (Proxy BondFullName) where
   column _ _ = "value"
+
+instance HasColumn "bondtradeagg" (Proxy Symbol) where
+  column _ _ = "symbol"
+
+newtype TradeAggDate = TradeAggDate Day
+                       deriving (Eq,Ord,Show,Data,Generic)
+
+instance Newtype TradeAggDate Day
+
+newtype TradeAggVol  = TradeAggVol (Maybe Scientific)
+                       deriving (Eq,Ord,Show,Data,Generic)
+
+instance Newtype TradeAggVol (Maybe Scientific)
+
+newtype TradeAggNum  = TradeAggNum  Int
+                       deriving (Eq,Ord,Show,Data,Generic)
+
+instance Newtype TradeAggNum Int
+
+instance HasColumn "bondtradeagg" (Proxy TradeAggDate) where
+  column _ _  = "day"
+
+instance HasColumn "bondtradeagg" (Proxy TradeAggVol) where
+  column _ _  = "vol"
+
+instance HasColumn "bondtradeagg" (Proxy TradeAggNum) where
+  column _ _  = "trades"
 
 
