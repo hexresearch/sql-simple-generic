d92ed20d08645e32dfe004ab2321426f1564f6c6
Dmitry Zuikov <dzuikov@gmail.com>
2021-01-06 08:22:56 +0300

    minor refactoring

--- DB/PG.hs
+++ DB/PG.hs
@@ -1,10 +1,15 @@
+{-# LANGUAGE QuasiQuotes, ExtendedDefaultRules #-}
 module DB.PG ( module DB
              , module DB.PG
              ) where
 
-import Data.String (IsString(..))
-import Data.ByteString (ByteString)
 import Database.PostgreSQL.Simple
+import Data.ByteString (ByteString)
+import Data.Proxy
+import Data.String (IsString(..))
+import GHC.TypeLits
+import qualified Data.Text as Text
+import Text.InterpolatedString.Perl6 (qc)
 
 import DB
 
@@ -34,4 +39,58 @@ disposeEngine :: PostgreSQLEngine -> IO ()
 disposeEngine e = do
   conn <- getConnection e
   close conn
+
+
+data From (table :: Symbol) cols = All
+data Rows cols
+
+type family SelectRowType a :: *
+type instance SelectRowType (From t (Rows [(a1,a2)])) = (a1,a2)
+type instance SelectRowType (From t (Rows [(a1,a2,a3)])) = (a1,a2,a3)
+type instance SelectRowType (From t (Rows [(a1,a2,a3,a4)])) = (a1,a2,a3,a4)
+
+instance ( HasTable   (From t (Rows [row])) PostgreSQLEngine
+         , HasColumns (From t (Rows [row]))
+         , FromRow row
+         ) => SelectStatement (From t (Rows [row])) IO PostgreSQLEngine
+      where
+        type SelectResult (From t (Rows [row])) = [row]
+        select eng q = do
+          conn <- getConnection eng
+          query_ conn [qc|select {cols} from {table}|]
+          where table = tablename eng q
+                cols  = Text.intercalate "," (columns q)
+
+instance ( HasColumn t (Proxy a)
+         , HasColumn t (Proxy b)
+         , KnownSymbol t
+         ) => HasColumns (From t (Rows [(a, b)])) where
+  columns = const [ column (Proxy @t) (Proxy @a), column (Proxy @t) (Proxy @b) ]
+
+
+instance ( HasColumn t (Proxy a1)
+         , HasColumn t (Proxy a2)
+         , HasColumn t (Proxy a3)
+         , KnownSymbol t
+         ) => HasColumns (From t (Rows [(a1, a2, a3)])) where
+
+  columns = const [ column (Proxy @t) (Proxy @a1)
+                  , column (Proxy @t) (Proxy @a2)
+                  , column (Proxy @t) (Proxy @a3)
+                  ]
+
+
+instance ( HasColumn t (Proxy a1)
+         , HasColumn t (Proxy a2)
+         , HasColumn t (Proxy a3)
+         , HasColumn t (Proxy a4)
+         , KnownSymbol t
+         ) => HasColumns (From t (Rows [(a1, a2, a3, a4)])) where
+
+  columns = const [ column (Proxy @t) (Proxy @a1)
+                  , column (Proxy @t) (Proxy @a2)
+                  , column (Proxy @t) (Proxy @a3)
+                  , column (Proxy @t) (Proxy @a4)
+                  ]
+
 
--- DB/PG/Instances.hs
+++ DB/PG/Instances.hs
@@ -65,57 +65,6 @@ instance ToField Price where
 instance ToField Volume where
   toField (Volume x) = toField (realToFrac x :: Scientific)
 
-data From (table :: T.Symbol) cols = All
-data Rows cols
-
-type family SelectRowType a :: *
-type instance SelectRowType (From t (Rows [(a1,a2)])) = (a1,a2)
-type instance SelectRowType (From t (Rows [(a1,a2,a3)])) = (a1,a2,a3)
-type instance SelectRowType (From t (Rows [(a1,a2,a3,a4)])) = (a1,a2,a3,a4)
-
-instance ( HasTable   (From t (Rows [row])) PostgreSQLEngine
-         , HasColumns (From t (Rows [row]))
-         , FromRow row
-         ) => SelectStatement (From t (Rows [row])) IO PostgreSQLEngine
-      where
-        type SelectResult (From t (Rows [row])) = [row]
-        select eng q = do
-          conn <- getConnection eng
-          query_ conn [qc|select {cols} from {table}|]
-          where table = tablename eng q
-                cols  = Text.intercalate "," (columns q)
-
-instance ( HasColumn t (Proxy a)
-         , HasColumn t (Proxy b)
-         , KnownSymbol t
-         ) => HasColumns (From t (Rows [(a, b)])) where
-  columns = const [ column (Proxy @t) (Proxy @a), column (Proxy @t) (Proxy @b) ]
-
-
-instance ( HasColumn t (Proxy a1)
-         , HasColumn t (Proxy a2)
-         , HasColumn t (Proxy a3)
-         , KnownSymbol t
-         ) => HasColumns (From t (Rows [(a1, a2, a3)])) where
-
-  columns = const [ column (Proxy @t) (Proxy @a1)
-                  , column (Proxy @t) (Proxy @a2)
-                  , column (Proxy @t) (Proxy @a3)
-                  ]
-
-
-instance ( HasColumn t (Proxy a1)
-         , HasColumn t (Proxy a2)
-         , HasColumn t (Proxy a3)
-         , HasColumn t (Proxy a4)
-         , KnownSymbol t
-         ) => HasColumns (From t (Rows [(a1, a2, a3, a4)])) where
-
-  columns = const [ column (Proxy @t) (Proxy @a1)
-                  , column (Proxy @t) (Proxy @a2)
-                  , column (Proxy @t) (Proxy @a3)
-                  , column (Proxy @t) (Proxy @a4)
-                  ]
 
 instance KnownSymbol t => HasTable (From t cols) e where
   tablename _ _ = fromString (symbolVal (Proxy @t))
